49175b53bad63f5166c0ed3d01d8332a7310ee31
fecodebr <fecoder.dev@gmail.com>
2023-04-04 20:43:44 -0300

    Atualizações: conclusão aula

--- modulo03-produtividade-com-angular/src/Components/CardNews.js
+++ modulo03-produtividade-com-angular/src/Components/CardNews.js
@@ -1,5 +1,5 @@
 class CardNews extends HTMLElement {
-  //Método construtor
+  // Método construtor
   constructor() {
     super();
 
@@ -7,40 +7,42 @@ class CardNews extends HTMLElement {
     shadow.appendChild(this.build());
     shadow.appendChild(this.styles());
   }
+
+  // Método build
   build() {
     const componentRoot = document.createElement("div");
     componentRoot.setAttribute("class", "card");
 
-    //div card__left: imagem
+    // div card__left: imagem
     const cardLeft = document.createElement("div");
     cardLeft.setAttribute("class", "card__left");
 
     const newsImage = document.createElement("img");
-    //img
+    // img
     newsImage.src = this.getAttribute("photo") || "assests/photo-default.jpg";
-    newsImage.alt = "Foto da Noíticia";
+    newsImage.alt = "Foto da Notícia";
     cardLeft.appendChild(newsImage);
 
-    //Div autor, título e notícia.
+    // Div autor, título e notícia.
     const autorNoticia = document.createElement("span");
     autorNoticia.textContent =
       "By " + (this.getAttribute("autorNoticia") || "Anonymous");
 
-    //Url
+    // Url
     const linkTitle = document.createElement("a");
     linkTitle.textContent = " " + this.getAttribute("title");
     linkTitle.href = this.getAttribute("link-url");
 
-    //Parágrafo
+    // Parágrafo
     const paragrafoNoticia = document.createElement("p");
-    paragrafoNoticia.textContent = this.getAttribute("content");
+    paragrafoNoticia.textContent = this.getAttribute("content"); 
 
     const cardRight = document.createElement("div");
     cardRight.setAttribute("class", "card__right");
 
     cardRight.appendChild(autorNoticia);
     cardRight.appendChild(linkTitle);
-    cardRight.appendChild(paragrafoNoticia);
+    cardRight.appendChild(paragrafoNoticia);    
 
     componentRoot.appendChild(cardLeft);
     componentRoot.appendChild(cardRight);
@@ -53,15 +55,17 @@ class CardNews extends HTMLElement {
     const style = document.createElement("style");
 
     style.textContent = `
-    
+
     .card {
-      width: 80%;
-      box-shadow: 8px 14px 30px -8px rgba(0, 0, 0, 0.75);
-      -webkit-box-shadow: 8px 14px 30px -8px rgba(0, 0, 0, 0.75);
-      -moz-box-shadow: 8px 14px 30px -8px rgba(0, 0, 0, 0.75); /* sombra em volta da caixa */
+      width: 100%;
+      box-shadow: 10px 10px 5px -5px rgba(131,247,165,0.75);
+      -webkit-box-shadow: 10px 10px 5px -5px rgba(131,247,165,0.75);
+      -moz-box-shadow: 10px 10px 5px -5px rgba(131,247,165,0.75); /* sombra em volta da caixa */
       display: flex;
       flex-direction: row;
       justify-content: space-between;
+      margin: 10px 0 30px 0;
+      padding: 0 0 5px 0;
     }
     
     .card__left img {
@@ -76,7 +80,8 @@ class CardNews extends HTMLElement {
       flex-direction: column;
       justify-content: center;
       text-align: left;
-      padding-right: 20px;
+      padding-right: 10px;
+      margin: 0 10px;
     }
     
     .card__left {
@@ -89,15 +94,16 @@ class CardNews extends HTMLElement {
     .card__right > a {
       margin: 10px 5px 5px 10px;
       font-size: 15px;
-      padding: 5px;
+      padding: 5px;      
       text-decoration: none;
-      color: #000;
+      color: #A99877;
     }
     
     .card__right > span,
     .card__right > p {
-      margin: 2px 0 0 10px;
-      padding: 10px;
+      margin: 10px 5px 0 10px;
+      padding: 10px 5px; 
+      color: #A99877;
     }
     
     .card__right > span {
@@ -105,7 +111,7 @@ class CardNews extends HTMLElement {
     }
     
     .card__right > p {
-      color: #555151;
+      color: #A99877;
     }
 
   `;
