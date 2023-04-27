dc24d905be9c4cd011b07dfe2675d2fa45d0ea99
fecodebr <fecoder.dev@gmail.com>
2023-04-27 04:09:44 -0300

    :white_check_mark: Conccluído aula sobre diretivas estruturais
    Anotações para revisão no notion

---
+++ modulo03-produtividade-com-angular/06-single-page-application-com-angular/Diretivas/diretivas-project/src/app/card/card.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent implements OnInit {
+
+  produtos:string[] = [];
+
+  menuType:string = "user-random";
+
+  constructor() {
+    this.produtos = ['mouse', 'teclado', 'cabos', 'fonte'];
+  }
+
+  ngOnInit(): void {}
+
+  adicionar() {
+    this.produtos.push('fulano');
+  }
+
+  remover(index: number) {
+    this.produtos.splice(index, 1);
+  }
+}
